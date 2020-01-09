fbd734c5d484797d82edb512c0132e1aac170af1
kurakin.as <aleksandr.kurakin@domru.ru>
2020-01-09 10:49:40 +0500

    Added comments for singleton

--- singleton/src/main/java/SimpleSingleton.java
+++ singleton/src/main/java/SimpleSingleton.java
@@ -1,7 +1,22 @@
+/**
+ * SimpleSingleton implements Singleton pattern.
+ * Static instance is initialized eagerly that's why thread safety guaranteed.
+ */
 public final class SimpleSingleton {
+    /**
+     * This is private constructor. Nobody can make instance of class.
+     */
     private SimpleSingleton() {}
 
+    /**
+     * Static final instance of class.
+     */
     private static final SimpleSingleton INSTANCE = new SimpleSingleton();
 
+    /**
+     * This method is used for getting instance of class by users.
+     *
+     * @return instance of the SimpleSingleton.
+     */
     public static SimpleSingleton getInstance() { return INSTANCE; }
 }
