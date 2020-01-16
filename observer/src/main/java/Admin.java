868c397d25f08a2862701a373141a61ddb9876e0
kurakin.as <aleksandr.kurakin@domru.ru>
2020-01-16 18:07:22 +0500

    Added observer example

---
+++ observer/src/main/java/Admin.java
@@ -0,0 +1,10 @@
+import java.util.logging.Logger;
+
+public class Admin implements ChatObserver {
+    private static final Logger LOGGER = Logger.getLogger(Admin.class.getName());
+
+    @Override
+    public void update(String message) {
+        LOGGER.info(String.format("%s get new message: %s", this.getClass().getName(), message));
+    }
+}
---
+++ observer/src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        User user = new User();
+        Admin admin = new Admin();
+
+        Chat chat = new Chat();
+        chat.addObserver(admin);
+        chat.addObserver(user);
+
+        chat.addMessage("Hi");
+        chat.addMessage("Hello");
+        chat.addMessage("Buy");
+    }
+}
